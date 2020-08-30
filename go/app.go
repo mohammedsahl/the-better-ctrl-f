2b59c6bf6a21cf739ce09813e1a892aab1a81c42
Mohammed Sahl <sahl.kably@gmail.com>
2020-08-30 19:02:58 -0400

    Add .env to gitignore

--- go/app.go
+++ go/app.go
@@ -15,9 +15,13 @@ func main() {
 	c.Tabs.Create(tabDetails, func(tab chrome.Tab) {
 		notification := "Tab with id: " + strconv.Itoa(tab.Id) + " created!"
 		Document.GetElementById("notification").SetInnerHTML(notification)
+
+		//TODO: Remove from Tabs.Create
+		// Word stored in word
+		// Values are a <p> tag
 		values := Document.GetElementById("values")
 		word := Document.GetElementById("word")
-	
+
 		word.AddEventListener("keyup", func(e Event) {
 			if (e.Key() == "Enter") {
 				values.Set("textContent", e.Target().Value())
